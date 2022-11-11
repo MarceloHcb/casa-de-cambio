f5813c29f05e27071cd1fc3b586bd186b73127eb
MarceloHcb <marcelohcb@msn.com>
2022-11-10 22:49:31 -0300

    atualizando o codigo

--- src/main.js
+++ src/main.js
@@ -4,37 +4,52 @@ import Swal from 'sweetalert2';
 const searchButton = document.getElementById('btn-search');
 const textInput = document.getElementById('text-input');
 const ul = document.querySelector('.coins');
-let coinsTitle = document.querySelector('.coins-title')
+const coinsTitle = document.querySelector('.coins-title');
 const API = 'https://api.exchangerate.host/latest?base=';
 
 function fetchAPI(coin) {
   return fetch(`${API}${coin}`)
     .then((response) => response.json())
-    .then((data) => data.rates);
+    .then((data) => {
+      if (data.base !== coin.toUpperCase()) {
+        throw new Error('Moeda não existente!');
+      }
+      return data.rates;
+    });
 }
 function renderCoins(coins) {
   ul.innerHTML = '';
-  
+
   const coinsArray = Object.entries(coins);
   coinsArray.forEach((coinn) => {
-
     const [coinName, value] = coinn;
     const li = document.createElement('li');
-    li.textContent = `${coinName} - ${value}`;
+    const span = document.createElement('span');
+    li.textContent = `${coinName} - `;
+    span.textContent = `${value}`;
     ul.appendChild(li);
+    li.appendChild(span);
   });
 }
-searchButton.addEventListener('click', () => {
-    const coin = textInput.value;
-    coinsTitle.innerHTML = `Valores referentes a 1 ${coin.toUpperCase()}`
-    if(!coin) {
-        return Swal.fire({
-            icon: 'error',
-            title: 'Opsss',
-            text: 'Você precisa digitar uma moeda',
-        });
-    }
+function handleSearch() {
+  const coin = textInput.value.toUpperCase();
+  coinsTitle.innerHTML = `Valores referentes a 1 ${coin.toUpperCase()}`;
+  if (!coin) {
+    return Swal.fire({
+      icon: 'error',
+      title: 'Opsss...',
+      text: 'Você precisa digitar uma moeda',
+    });
+  }
 
   fetchAPI(coin)
-  .then(renderCoins);
-});
+    .then(renderCoins)
+    .catch((error) => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Opsss...',
+        text: error.message,
+      });
+    });
+}
+searchButton.addEventListener('click', handleSearch);
